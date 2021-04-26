2b7c54dc549d04114b64edde397d07819d3ef9fe
Marko Cekaj <marko.cekaj@amplitudo.me>
2021-04-26 13:21:43 +0200

    minor change

--- src/components/idCard/IdCard.jsx
+++ src/components/idCard/IdCard.jsx
@@ -3,26 +3,24 @@ import Image from "../image/image";
 import "./idcard.css";
 const IdCard = ({ name, surname, date, city, src }) => {
   return (
-    <div className="id">
-      <div className="idContainer">
-        <div className="idWrapper">
-          <div className="idInfo">
-            <div>
-              <strong>Name:</strong> {name}
-            </div>
-            <div>
-              <strong>Surname:</strong> {surname}
-            </div>
-            <div>
-              <strong>Born:</strong> {date}
-            </div>
-            <div>
-              <strong>City:</strong> {city}
-            </div>
+    <div className="idContainer">
+      <div className="idWrapper">
+        <div className="idInfo">
+          <div>
+            <strong>Name:</strong> {name}
           </div>
-          <div className="idImage">
-            <Image src={src} />
+          <div>
+            <strong>Surname:</strong> {surname}
           </div>
+          <div>
+            <strong>Born:</strong> {date}
+          </div>
+          <div>
+            <strong>City:</strong> {city}
+          </div>
+        </div>
+        <div className="idImage">
+          <Image src={src} />
         </div>
       </div>
     </div>
